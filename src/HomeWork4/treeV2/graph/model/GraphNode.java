83e51bd058c3fb6807f44e15292657ece94a22e0
DIS686 <106627508+maksimd31@users.noreply.github.com>
2023-04-07 17:32:28 +0300

    home work 4 down

---
+++ src/HomeWork4/treeV2/graph/model/GraphNode.java
@@ -0,0 +1,16 @@
+package HomeWork4.treeV2.graph.model;
+
+import java.util.ArrayList;
+
+public class GraphNode {
+    ArrayList<GraphNode> neighbors = new ArrayList<>();
+    boolean hasObject;
+
+    public ArrayList<GraphNode> getNeighbors() {
+        return neighbors;
+    }
+
+    public boolean hasObject() {
+        return hasObject;
+    }
+}
